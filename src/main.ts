530b33cc7731741ddad0805e1f0ef608f0840236
Hiromune Ito <muniere.fujiwara@gmail.com>
2022-01-19 01:27:31 +0900

    Add 005-space-invader

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as starField from './001-star-field';
 import * as mengerSponge from './002-menger-sponge';
 import * as snakeGame from './003-snake-game';
 import * as purpleRain from './004-purple-rain';
+import * as spaceInvader from './005-space-invader';
 import * as dithering from './090-dithering';
 
-new p5(purpleRain.sketch);
+new p5(spaceInvader.sketch);
