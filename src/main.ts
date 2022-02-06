d27df1d24e307af473819421b5985d0f8388529a
Hiromune Ito <muniere.fujiwara@gmail.com>
2022-02-06 17:24:32 +0900

    Re-layout files

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,6 @@
 // @ts-ignore
 import p5 from "p5";
 
-import * as starField from './001-star-field';
-import * as mengerSponge from './002-menger-sponge';
-import * as snakeGame from './003-snake-game';
-import * as purpleRain from './004-purple-rain';
-import * as spaceInvader from './005-space-invader';
-import * as mitosis from './006-mitosis';
-import * as solarSystem2D from './007-solar-system-2d';
-import * as reactionDiffusion from './013-reaction-diffusion';
-import * as fractalRecursive from './014-fractal-recursive';
-import * as fractalObjective from './015-fractal-objective';
-import * as travelingSalesperson from './035-traveling-salesperson';
-import * as markovChain from './042-markov-chain';
-import * as contextFreeGrammer from './043-context-free-grammer';
-import * as circlePacking from './050-circle-packing';
-import * as pathfinding from './051-pathfinding';
-import * as nearestNeighbors from './070-nearest-neighbors';
-import * as circleMorphing from './081-circle-morphing';
-import * as gameOfLife from './085-game-of-life';
-import * as dithering from './090-dithering';
-import * as sortVisualization from './114-sort-visualization';
-import * as sevenSegment from './117-seven-segment';
-import * as fourierSeries from './125-fourier-series';
-import * as fourierTransform from './130-fourier-transform';
-import * as ticTacToe from './149-tic-tac-toe';
-import * as parrondoParadox from './_01-parrondo-paradox';
+import { sketch } from './apps/085-game-of-life';
 
-new p5(gameOfLife.sketch);
+new p5(sketch);
