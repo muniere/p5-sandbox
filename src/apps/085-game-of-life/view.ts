a3bea83ae11ef16f9102fe06a54e43b2de21997e
Hiromune Ito <muniere.fujiwara@gmail.com>
2022-02-23 18:42:04 +0900

    Optimize drawing process of 085-game-of-life

--- src/apps/085-game-of-life/view.ts
+++ src/apps/085-game-of-life/view.ts
@@ -16,27 +16,38 @@ export class ApplicationWidget extends Widget<ApplicationModel> {
     });
 
     this.scope($ => {
+      $.fill(this.deadColor);
+      $.rect(this.frame.left, this.frame.top, this.frame.width, this.frame.height);
+    });
+
+    this.scope($ => {
+      $.fill(this.aliveColor);
+      $.noStroke();
+
       model.grid.walk((state: State, spot: Spot) => {
+        if (state == State.dead) {
+          return;
+        }
+
         const origin = Point.of({
           x: this.frame.origin.x + spot.column * cellSize.width,
           y: this.frame.origin.y + spot.row * cellSize.height,
         });
 
-        const strokeColor = this.borderColor;
-
-        const fillColor = (() => {
-          switch (state) {
-            case State.alive:
-              return this.aliveColor;
-            case State.dead:
-              return this.deadColor;
-          }
-        })();
-
-        $.stroke(strokeColor);
-        $.fill(fillColor);
         $.rect(origin.x, origin.y, cellSize.width, cellSize.height);
       });
     });
+
+    this.scope($ => {
+      $.stroke(this.borderColor);
+      $.noFill();
+
+      for (let x = cellSize.width; x < this.frame.width; x += cellSize.width) {
+        $.line(x, 0, x, this.frame.height);
+      }
+      for (let y = cellSize.height; y < this.frame.height; y += cellSize.height) {
+        $.line(0, y, this.frame.width, y);
+      }
+    })
   }
 }
